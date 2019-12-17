2e78ce2677f61556442b6b73dd50c05bea4f6b90
Wayne Hofstra <wayne.hofstra@hva.nl>
2019-12-17 14:57:24 +0100

    Weapon Functionality Seperated

---
+++ Nuclear Throne/Assets/Scripts/Weapons/WeaponFunctions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponFunctions : MonoBehaviour
+{
+    private bool automatic;
+
+    void Start()
+    {
+        automatic = false;
+    }
+
+    void Update()
+    {
+        InputButtons();
+    }
+
+    private void InputButtons()
+    {
+        //Left Mouse-button Click: Fire
+        if (Input.GetButtonDown("Fire1") && !automatic)
+        {
+            Debug.Log("Pang!");
+        }
+        else if (Input.GetButton("Fire1") && automatic)
+        {
+            Debug.Log("Prrangpangang!");
+        }
+
+        //E-key Press: Weapon Switch
+        if (Input.GetButtonDown("Switch"))
+        {
+            if (automatic)
+            {
+                automatic = false;
+            }
+            else
+            {
+                automatic = true;
+            }
+        }
+    }
+}
