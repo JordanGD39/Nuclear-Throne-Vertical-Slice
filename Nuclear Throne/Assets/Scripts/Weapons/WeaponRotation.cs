2af6a799fcaf585324042532a27b1fe2f95e87fa
Wayne Hofstra <wayne.hofstra@hva.nl>
2019-12-18 12:43:22 +0100

    Weapon Rotation and Layer Change

--- Nuclear Throne/Assets/Scripts/Weapons/WeaponRotation.cs
+++ Nuclear Throne/Assets/Scripts/Weapons/WeaponRotation.cs
@@ -8,20 +8,45 @@ public class WeaponRotation : MonoBehaviour
     [SerializeField]
     private RectTransform _canvas;
 
-    [SerializeField]
-    private Camera _camera;
+    private GameObject player;
+    private SpriteRenderer childObj;
 
-    void Start()
+    private void Start()
     {
-        Vector2 mousePos = _camera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotationZ = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        player = GameObject.FindGameObjectWithTag("Player");
+        childObj = transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         //Weapon Rotation with the Mouse-position
-        Vector2 mousePos = _camera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotationZ = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotationZ - 90);
+
+        transform.position = player.transform.position;
+
+        ChangeSortingLayer(player.transform.localScale.x, transform.rotation.eulerAngles.z);
+    }
+
+    private void ChangeSortingLayer(float xScale, float zAngle)
+    {
+        if (xScale < 0)
+        {
+            childObj.flipX = true;
+        }
+        else if (xScale > 0)
+        {
+            childObj.flipX = false;
+        }
+
+        if (zAngle < 90 || zAngle > 270)
+        {
+            childObj.sortingOrder = -1;
+        }
+        else
+        {
+            childObj.sortingOrder = 1;
+        }
     }
 }
