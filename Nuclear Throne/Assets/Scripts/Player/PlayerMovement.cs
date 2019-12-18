d1747d49250d09c1e2187d88f98b83a59b07e279
Wayne Hofstra <wayne.hofstra@hva.nl>
2019-12-18 14:37:53 +0100

    Player Flips with Cursor

--- Nuclear Throne/Assets/Scripts/Player/PlayerMovement.cs
+++ Nuclear Throne/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Transform cursor;
+
     [SerializeField] private float speed;
+
     private Vector2 movement;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        cursor = GameObject.FindGameObjectWithTag("Cursor").GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -20,11 +24,11 @@ public class PlayerMovement : MonoBehaviour
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         movement = movement.normalized;
 
-        if (movement.x > 0)
+        if ((transform.position.x - cursor.position.x) < 0)
         {
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, 1);
         }
-        else if(movement.x < 0)
+        else if((transform.position.x - cursor.position.x) > 0)
         {
             transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, 1);
         }
