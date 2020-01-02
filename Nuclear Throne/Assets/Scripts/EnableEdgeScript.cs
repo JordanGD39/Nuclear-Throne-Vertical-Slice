5e52f890b6ad69e2e678000a911dbfa40c90bdff
JordanG <26260@ma-web.nl>
2020-01-03 00:44:05 +0100

    explosion added

---
+++ Nuclear Throne/Assets/Scripts/EnableEdgeScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableEdgeScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            if (!collision.GetComponent<SpriteEdgeCheck>().enabled)
+            {
+                collision.GetComponent<SpriteEdgeCheck>().enabled = true;
+                collision.GetComponent<SpriteEdgeCheck>().CheckedEdge = false;
+            }
+        }
+    }
+}
--- Nuclear Throne/Assets/Scripts/SpriteEdgeCheck.cs
+++ Nuclear Throne/Assets/Scripts/SpriteEdgeCheck.cs
@@ -6,22 +6,24 @@ public class SpriteEdgeCheck : MonoBehaviour
 {
     [SerializeField] private GameObject[] sprites;
 
-    private bool checkedEdge = false;
+    public bool CheckedEdge { get; set; } = false;
 
     private void Start()
     {
-        checkedEdge = false;
+        CheckedEdge = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!checkedEdge)
+        if (!CheckedEdge)
         {
-            RaycastHit2D hitUp = Physics2D.Raycast(transform.position + new Vector3(0, 0.6f, 0), Vector2.up, 0.1f);
-            RaycastHit2D hitDown = Physics2D.Raycast(transform.position - new Vector3(0, 0.6f, 0), -Vector2.up, 0.1f);
-            RaycastHit2D hitLeft = Physics2D.Raycast(transform.position - new Vector3(0.6f, 0, 0), -Vector2.right, 0.1f);
-            RaycastHit2D hitRight = Physics2D.Raycast(transform.position + new Vector3(0.6f, 0, 0), Vector2.right, 0.1f);
+            int tileLayer = LayerMask.GetMask("Tile");
+
+            RaycastHit2D hitUp = Physics2D.Raycast(transform.position + new Vector3(0, 0.6f, 0), Vector2.up, 0.1f, tileLayer);
+            RaycastHit2D hitDown = Physics2D.Raycast(transform.position - new Vector3(0, 0.6f, 0), -Vector2.up, 0.1f, tileLayer);
+            RaycastHit2D hitLeft = Physics2D.Raycast(transform.position - new Vector3(0.6f, 0, 0), -Vector2.right, 0.1f, tileLayer);
+            RaycastHit2D hitRight = Physics2D.Raycast(transform.position + new Vector3(0.6f, 0, 0), Vector2.right, 0.1f, tileLayer);
 
             if (hitUp.collider == null)
             {                
@@ -43,8 +45,7 @@ public class SpriteEdgeCheck : MonoBehaviour
                 Instantiate(sprites[3], gameObject.transform, false);
             }
 
-            checkedEdge = true;
-
+            CheckedEdge = true;
             GetComponent<SpriteEdgeCheck>().enabled = false;
         }
     }
