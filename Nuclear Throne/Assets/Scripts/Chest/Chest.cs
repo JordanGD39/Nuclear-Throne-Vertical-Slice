8eb3cbf01915ed0be8bba47ffdb8cdd4b9020b9e
JordanG <26260@ma-web.nl>
2020-01-16 12:46:10 +0100

    Chest gives ammo and based on if you have max ammo

--- Nuclear Throne/Assets/Scripts/Chest/Chest.cs
+++ Nuclear Throne/Assets/Scripts/Chest/Chest.cs
@@ -13,9 +13,9 @@ public class Chest : MonoBehaviour
 
     [SerializeField] private GameObject pickUpPref;
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && Input.GetButtonDown("Pickup"))
+        if (collision.CompareTag("Player"))
         {
             GetComponent<BoxCollider2D>().enabled = false;
 
@@ -49,103 +49,193 @@ public class Chest : MonoBehaviour
                     GiveAmmo(collision, weapon.WeaponOfGameObject);
                 }
             }
+            else
+            {
+                StatsClass player = collision.GetComponent<StatsClass>();
+
+                Weapon weapon = null;
+
+                int rand = Random.Range(0, 2);
+
+                if (multiplier == 1)
+                {
+                    if (rand == 0)
+                    {
+                        weapon = player.Primary;
+                    }
+                    else
+                    {
+                        weapon = player.Secondary;
+                    }
+                }
+                else
+                {
+                    weapon = player.Primary;
+
+                    if (weapon.Melee)
+                    {
+                        weapon = player.Secondary;
+                    }
+                }
+
+                switch (weapon.WeaponBullet.bulletType)
+                {
+                    case Bullet.ammoType.NORMAL:
+                        if (player.Ammo == 120 && player.GetComponent<Roll>() != null || player.Ammo == 99 && player.GetComponent<Roll>() == null || weapon.Melee)
+                        {
+                            weapon = new Weapon();
+                            Bullet bullet = new Bullet();
+
+                            bullet.bulletType = (Bullet.ammoType)Random.Range(0, 4);
+
+                            weapon.WeaponBullet = bullet;
+                        }
+                        break;
+                    case Bullet.ammoType.SHELL:
+                        if (player.ShellAmmo == 120 && player.GetComponent<Roll>() != null || player.ShellAmmo == 99 && player.GetComponent<Roll>() == null || weapon.Melee)
+                        {
+                            weapon = new Weapon();
+                            Bullet bullet = new Bullet();
+
+                            bullet.bulletType = (Bullet.ammoType)Random.Range(0, 4);
+
+                            weapon.WeaponBullet = bullet;
+                        }
+                        break;
+                    case Bullet.ammoType.ENERGY:
+                        if (player.EnergyAmmo == 120 && player.GetComponent<Roll>() != null || player.EnergyAmmo == 99 && player.GetComponent<Roll>() == null || weapon.Melee)
+                        {
+                            weapon = new Weapon();
+                            Bullet bullet = new Bullet();
+
+                            bullet.bulletType = (Bullet.ammoType)Random.Range(0, 4);
+
+                            weapon.WeaponBullet = bullet;
+                        }
+                        break;
+                    case Bullet.ammoType.EXPLOSION:
+                        if (player.ExplosiveAmmo == 120 && player.GetComponent<Roll>() != null || player.ExplosiveAmmo == 99 && player.GetComponent<Roll>() == null || weapon.Melee)
+                        {
+                            weapon = new Weapon();
+                            Bullet bullet = new Bullet();
+
+                            bullet.bulletType = (Bullet.ammoType)Random.Range(0, 4);
+
+                            weapon.WeaponBullet = bullet;
+                        }
+                        break;
+                    case Bullet.ammoType.BOLT:
+                        if (player.BoltAmmo == 120 && player.GetComponent<Roll>() != null || player.BoltAmmo == 99 && player.GetComponent<Roll>() == null || weapon.Melee)
+                        {
+                            weapon = new Weapon();
+                            Bullet bullet = new Bullet();                            
+
+                            bullet.bulletType = (Bullet.ammoType)Random.Range(0, 4);
+
+                            weapon.WeaponBullet = bullet;
+                        }
+                        break;
+
+                }
+
+                GiveAmmo(collision, weapon);
+            }
         }
     }
 
     private void GiveAmmo(Collider2D collision, Weapon weapon)
     {
-        bool norm = false;
-        bool shell = false;
-        bool bolt = false;
-        bool explosive = false;
-        bool energy = false;
-
         StatsClass player = collision.GetComponent<StatsClass>();
-
-        switch (weapon.WeaponBullet.fireType)
-        {
-            case Bullet.type.NORMAL:
-                norm = true;
-                break;
-            case Bullet.type.FIRE:
-                norm = true;
-                break;
-            case Bullet.type.LASER:
-                energy = true;
-                break;
-            case Bullet.type.PLASMA:
-                energy = true;
-                break;
-            case Bullet.type.LIGHTNING:
-                energy = true;
-                break;
-            case Bullet.type.EXPLOSION:
-                explosive = true;
-                break;
-            case Bullet.type.MISSILE:
-                explosive = true;
-                break;
-            case Bullet.type.SEEKER:
-                explosive = true;
-                break;
-            case Bullet.type.DISC:
-                norm = true;
-                break;
-            case Bullet.type.SLUG:
-                shell = true;
-                break;
-            case Bullet.type.SHELL:
-                shell = true;
-                break;
-            case Bullet.type.BOLT:
-                bolt = true;
-                break;
-        }
-
         if (player.GetComponent<Roll>() != null)
         {
-            if (norm)
+            switch (weapon.WeaponBullet.bulletType)
             {
-                player.Ammo += 40 * multiplier;
-            }
-            else if (shell)
-            {
-                player.ShellAmmo += 10 * multiplier;
-            }
-            else if (bolt)
-            {
-                player.BoltAmmo += 9 * multiplier;
-            }
-            else if (explosive)
-            {
-                player.ExplosiveAmmo += 8 * multiplier;
-            }
-            else if (energy)
-            {
-                player.EnergyAmmo += 13 * multiplier;
+                case Bullet.ammoType.NORMAL:
+                    player.Ammo += 40 * multiplier;
+
+                    if (player.Ammo > 120)
+                    {
+                        player.Ammo = 120;
+                    }
+                    break;
+                case Bullet.ammoType.SHELL:
+                    player.ShellAmmo += 10 * multiplier;
+                    if (player.ShellAmmo > 120)
+                    {
+                        player.ShellAmmo = 120;
+                    }
+                    break;
+                case Bullet.ammoType.ENERGY:
+                    player.EnergyAmmo += 10 * multiplier;
+
+                    if (player.EnergyAmmo > 120)
+                    {
+                        player.EnergyAmmo = 120;
+                    }
+                    break;
+                case Bullet.ammoType.EXPLOSION:
+                    player.ExplosiveAmmo += 8 * multiplier;
+
+                    if (player.ExplosiveAmmo > 120)
+                    {
+                        player.ExplosiveAmmo = 120;
+                    }
+                    break;
+                case Bullet.ammoType.BOLT:
+                    player.BoltAmmo += 7 * multiplier;
+
+                    if (player.BoltAmmo > 120)
+                    {
+                        player.BoltAmmo = 120;
+                    }
+                    break;
             }
         }
         else
         {
-            if (norm)
+            switch (weapon.WeaponBullet.bulletType)
             {
-                player.Ammo += 32 * multiplier;
-            }
-            else if (shell)
-            {
-                player.ShellAmmo += 8 * multiplier;
-            }
-            else if (bolt)
-            {
-                player.BoltAmmo += 7 * multiplier;
-            }
-            else if (explosive)
-            {
-                player.ExplosiveAmmo += 6 * multiplier;
-            }
-            else if (energy)
-            {
-                player.EnergyAmmo += 10 * multiplier;
+                case Bullet.ammoType.NORMAL:
+                    player.Ammo += 32 * multiplier;
+
+                    if (player.Ammo > 120)
+                    {
+                        player.Ammo = 120;
+                    }
+                    break;
+                case Bullet.ammoType.SHELL:
+                    player.ShellAmmo += 8 * multiplier;
+
+                    if (player.ShellAmmo > 120)
+                    {
+                        player.ShellAmmo = 120;
+                    }
+                    break;
+                case Bullet.ammoType.ENERGY:
+                    player.EnergyAmmo += 10 * multiplier;
+
+                    if (player.EnergyAmmo > 120)
+                    {
+                        player.EnergyAmmo = 120;
+                    }
+
+                    break;
+                case Bullet.ammoType.EXPLOSION:
+                    player.ExplosiveAmmo += 6 * multiplier;
+
+                    if (player.ExplosiveAmmo > 120)
+                    {
+                        player.ExplosiveAmmo = 120;
+                    }
+                    break;
+                case Bullet.ammoType.BOLT:
+                    player.BoltAmmo += 7 * multiplier;
+
+                    if (player.BoltAmmo > 120)
+                    {
+                        player.BoltAmmo = 120;
+                    }
+                    break;
             }
         }
     }
