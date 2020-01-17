208e0b577727eddac2b6b03542b54c3d356fc35b
JordanG <26260@ma-web.nl>
2020-01-17 12:45:51 +0100

    bullet fly away on the ground when shot

--- Nuclear Throne/Assets/Scripts/Weapon/ShootGun.cs
+++ Nuclear Throne/Assets/Scripts/Weapon/ShootGun.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ShootGun : MonoBehaviour
 {
+    [SerializeField] private GameObject bulletShotPref;
 
     public void Shoot(GameObject bulletPrefab, Bullet bullet, Weapon weapon, bool playerControl)
     {
@@ -20,6 +21,15 @@ public class ShootGun : MonoBehaviour
         bulletScript.Loaded = true;
         bulletScript.PlayerControl = playerControl;
         bulletObj.GetComponent<SpriteRenderer>().enabled = true;
-        bulletObj.GetComponent<SpriteRenderer>().sprite = bullet.SpriteOfBullet;        
+        bulletObj.GetComponent<SpriteRenderer>().sprite = bullet.SpriteOfBullet;
+        if (playerControl)
+        {
+            GameObject groundBullet = Instantiate(bulletShotPref, transform.parent.parent.GetChild(0).position, transform.rotation);
+            groundBullet.transform.Rotate(0, 0, Random.Range(-360, 361));
+            groundBullet.transform.GetChild(0).rotation = transform.rotation;
+            groundBullet.transform.GetChild(0).Rotate(0, 0, Random.Range(-70, 71));
+            groundBullet.GetComponent<Rigidbody2D>().AddForce(-groundBullet.transform.GetChild(0).up * 800);
+            Destroy(groundBullet, 6);
+        }
     }
 }
