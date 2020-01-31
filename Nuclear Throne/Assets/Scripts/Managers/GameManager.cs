3bbf5afde4086cead1911548aa1104f0d97445dc
JordanG <26260@ma-web.nl>
2020-01-31 21:42:42 +0100

    local pos instead op global pos

--- Nuclear Throne/Assets/Scripts/Managers/GameManager.cs
+++ Nuclear Throne/Assets/Scripts/Managers/GameManager.cs
@@ -139,7 +139,7 @@ public class GameManager : MonoBehaviour
         {
             textGameObject.GetComponent<Image>().enabled = false;
         }
-        textGameObject.transform.position = Vector3.zero;
+        textGameObject.transform.localPosition = Vector3.zero;
         Text text = textGameObject.transform.GetChild(0).GetComponent<Text>();
 
         while (!ao.isDone)
@@ -147,7 +147,6 @@ public class GameManager : MonoBehaviour
             float progress = Mathf.Clamp01(ao.progress / 0.9f);
             text.text = textToShow + (progress * 100).ToString("F0") + "%";
             yield return null;
-            textGameObject.transform.localPosition = Vector3.zero;
         }
     }
 }
